caed900c0b9e83eb03e5d17b7ff03cddc5bd4d7c
Aisha Nuryan <aishaptrn@student.telkomuniversity.ac.id>
2025-06-14 08:22:49 +0700

    Update index.blade.php

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -91,7 +91,7 @@
         function handleDelete(name) {
             Swal.fire({
                 title: "Are you sure?",
-                text: "You won't be able to revert this!",
+                text: "You will not be able to revert this!",
                 icon: "warning",
                 showCancelButton: true,
                 confirmButtonColor: "#3085d6",
